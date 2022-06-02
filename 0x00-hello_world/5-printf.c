00123628fdb79450204b2d8ac41d8f67d7c67e0b
fulani-bros <mbellosarkiwa@gmail.com>
2022-06-02 13:00:47 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,9 @@
 #include <stdio.h>
-
 /**
- *
- *  * main - beg
- *
- *   *
- *
- *    * Return: stop
- *
- *     */
-
+ * main - main block
+ * Return: 0
+ */
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
-}
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
