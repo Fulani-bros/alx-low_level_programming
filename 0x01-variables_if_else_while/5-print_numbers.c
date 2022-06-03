082402c11d428c9c23ba736fd22b0a6a3bc9d7bd
fulani-bros <mbellosarkiwa@gmail.com>
2022-06-03 06:48:48 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,13 +7,11 @@
  */
 int main(void)
 {
-	char i;
-
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		if (i != 'q' && i != 'e')
-			putchar(i);
-	}
-	putchar('\n');
-	return (0);
+	int i;
+	for (i = 0; i < 10; i++)
+{
+	printf("%d", i);
+}
+printf("\n");
+return (0);
 }
