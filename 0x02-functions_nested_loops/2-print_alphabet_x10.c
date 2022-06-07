aa3a2e0ca8961d6cb99ab27ae59676280c0c4d9d
fulani-bros <mbellosarkiwa@gmail.com>
2022-06-07 08:57:11 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,19 +3,18 @@
 /**
  *
  * print_alphabet_x10 - Prints the alphabet 10 times.
- *
- * Return: Always 0 (Success)
+ *i
  */
 void print_alphabet_x10(void)
 {
 	int i;
 	char j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
 		{
-			_putchar(b);
+			_putchar(j);
 		}
 		_putchar('\n');
 	}
