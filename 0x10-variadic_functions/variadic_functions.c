b9f8b32a5962283efc98cc67fa817ffd49e485be
fulani-bros <mbellosarkiwa@gmail.com>
2022-07-07 04:08:36 -0700

    Write a function that prints numbers, followed by a new line

--- 0x10-variadic_functions/variadic_functions.c
+++ 0x10-variadic_functions/variadic_functions.c
@@ -1,12 +1,21 @@
-#ifndef _VARIADIC_FUNCTIONS_
-#define _VARIADIC_FUNCTIONS_
-
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
-#include <stdio.h>
 
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+/**
+ * struct print - search the func for print_all
+ * @c: character to match
+ * @f: function for that character.
+ */
+typedef struct print
+{
+	char *c;
+	void (*f)();
+} print;
+
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
